95e29a4d9584a71d59e55b73a29cb07dd3a0425e
Eduardo Gutierrez <eduardo.gutierrez.2325@gmail.com>
2020-07-24 22:00:02 -0500

    Implemented task 1.2

---
+++ task1.2/app.js
@@ -0,0 +1,12 @@
+const csv = require("csvtojson");
+const fs = require("fs");
+const writer = fs.createWriteStream("./task1.2/output.txt");
+
+fs.createReadStream("./task1.2/csv/nodejs-hw1-ex1.csv")
+  .pipe(csv())
+  .on("data", function (row) {
+    writer.write(row);
+  })
+  .on("error", function (error) {
+    console.log("An error ocurred: ", error);
+  });
